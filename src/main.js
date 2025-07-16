6210ed6757627a26aa7be8684d68ce7a9bd1102c
”simobeijer” <beijer.simon@gmail.com>
2025-07-16 23:46:16 +0200

    feat: add vue-gtag for Google Analytics integration

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
+import VueGtag from 'vue-gtag'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+if (process.env.NODE_ENV === 'production') {
+  Vue.use(VueGtag, {
+    config: {
+      id: process.env.VUE_APP_GA_ID || 'G-87DVCF9NG0'
+    }
+  })
+}
 
 new Vue({
   render: h => h(App),
