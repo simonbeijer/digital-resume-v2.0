0c68954b5059dbf5a4d375ea22d4f1d4534eca1c
Simon Beijer <beijer.simon@gmail.com>
2025-12-10 18:29:04 +0100

    Added project

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -17,10 +17,10 @@
 </template>
 
 <script>
-import mli from '@/assets/mli.png';
 import matmind from '@/assets/matmind.png';
 import letter from '@/assets/personal-letter-LLM.png';
 import skooli from '@/assets/Skooli.png';
+import bandvault from '@/assets/bandvault.png';
 
 export default {
   name: 'Projects',
@@ -31,7 +31,7 @@ export default {
         { imgSrc: skooli, alt: 'Skooli', description: 'Tool for teachers to generate "Thematic learning" plans with the help of this AI/LLM RAG tool.', link: 'https://skooli-lilac.vercel.app/', linkText: 'Live link' },
         { imgSrc: letter, alt: 'Personal Letter LLM', description: 'Use this Large Laguage Model (AI) to create the perfect personal letter, combine the jobb ad and CV to create your personal letter.', link: 'https://personal-letter.vercel.app/', linkText: 'Live link' },
         { imgSrc: matmind, alt: 'Mat Mind', description: 'Online Jiu Jitsu coach. Mat Mind is a LLM (AI) driven expert that help you game on the mat i seconds.', link: 'https://mat-mind.vercel.app/', linkText: 'Live link' },
-        { imgSrc: mli, alt: 'MLI Lures', description: 'Responsive fishing lure e-commerce website built with modern web technologies. Features product showcase, responsive design, and optimized user experience.', link: 'https://simonbeijer.github.io/mli-build/', linkText: 'Github pages link' },
+        { imgSrc: bandvault, alt: 'Band Vault', description: 'BandVault is an app for bands to save and organize demo recordings. Each demo has chat and notes, easy to track ideas.', link: 'https://band-vault.vercel.app/', linkText: 'Live link' },
       ]
     };
   },
