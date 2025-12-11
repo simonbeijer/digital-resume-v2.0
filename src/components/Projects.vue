97b1accf13bf48b5c99676c4126cab1f3af393ff
Simon Beijer <beijer.simon@gmail.com>
2025-12-11 20:18:06 +0100

    Small change

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -29,9 +29,9 @@ export default {
       width: window.innerWidth,
       projects: [
         { imgSrc: skooli, alt: 'Skooli', description: 'Tool for teachers to generate "Thematic learning" plans with the help of this AI/LLM RAG tool.', link: 'https://skooli-lilac.vercel.app/', linkText: 'Live link' },
-        { imgSrc: letter, alt: 'Personal Letter LLM', description: 'Use this Large Laguage Model (AI) to create the perfect personal letter, combine the jobb ad and CV to create your personal letter.', link: 'https://personal-letter.vercel.app/', linkText: 'Live link' },
-        { imgSrc: matmind, alt: 'Mat Mind', description: 'Online Jiu Jitsu coach. Mat Mind is a LLM (AI) driven expert that help you game on the mat i seconds.', link: 'https://mat-mind.vercel.app/', linkText: 'Live link' },
         { imgSrc: bandvault, alt: 'Band Vault', description: 'BandVault is an app for bands to save and organize demo recordings. Each demo has chat and notes, easy to track ideas.', link: 'https://band-vault.vercel.app/', linkText: 'Live link' },
+        { imgSrc: matmind, alt: 'Mat Mind', description: 'Online Jiu Jitsu coach. Mat Mind is a LLM (AI) driven expert that help you game on the mat i seconds.', link: 'https://mat-mind.vercel.app/', linkText: 'Live link' },
+        { imgSrc: letter, alt: 'Personal Letter LLM', description: 'Use this Large Laguage Model (AI) to create the perfect personal letter, combine the jobb ad and CV to create your personal letter.', link: 'https://personal-letter.vercel.app/', linkText: 'Live link' },
       ]
     };
   },
